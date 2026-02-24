ece10ccab162e1d55979901bc71b09779a40d33a
Lev Dessyatykh <dessyatykh@gmail.com>
2026-02-24 08:32:48 +0500

    feat: Изменены записи на главную страницу через app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -93,16 +93,16 @@ import { FooterComponent } from './footer/footer.component';
   </header>
   <header class="welcome-section">
   <h1 class="main-title">
-  <span class="title-word title-1">Journal</span>
-  <span class="title-word title-2">Web</span>
-  <span class="title-word title-3">Arystan</span>
+  <span class="subtitle-word subtitle-1">Mission</span>
+  <span class="subtitle-word subtitle-2">Shekinah</span>
+  <span class="subtitle-word subtitle-3">Kazakstan</span>
   </h1>
   <p class="subtitle">
-  <span class="subtitle-word subtitle-1">Web</span>
-  <span class="subtitle-word subtitle-2">Development</span>
-  <span class="subtitle-word subtitle-3">Studio</span>
+  <span class="subtitle-word subtitle-1">The</span>
+  <span class="subtitle-word subtitle-2">School of Christ</span>
+  <span class="subtitle-word subtitle-3">International</span>
   </p>
-  </header>
+  </header>28
   </main>
 
   <app-footer></app-footer>
